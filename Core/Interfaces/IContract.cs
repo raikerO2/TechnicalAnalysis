ee7b2ee283916749c2ef2577cf5aa2645c4726b2
raikerO2 <raresmihaiiaut@gmail.com>
2021-04-23 16:12:16 +0100

    Added Types and Contract interface

---
+++ Core/Interfaces/IContract.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechnicalAnalysis.Core.Types;
+
+namespace TechnicalAnalysis.Core.Interfaces
+{
+    public interface IContract
+    {
+        Guid Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        int Age { get; set; }
+        string Email { get; set; }
+        ContractType Type { get; set; }
+    }
+}
