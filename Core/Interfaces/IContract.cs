0419d94413e3c257b0f20b8fa5485ac5aacd97fa
raikerO2 <raresmihaiiaut@gmail.com>
2021-04-23 16:56:03 +0100

    Added contracts

--- Core/Interfaces/IContract.cs
+++ Core/Interfaces/IContract.cs
@@ -6,13 +6,12 @@ using TechnicalAnalysis.Core.Types;
 
 namespace TechnicalAnalysis.Core.Interfaces
 {
-    public interface IContract
+    interface IContract
     {
         Guid Id { get; set; }
         string FirstName { get; set; }
         string LastName { get; set; }
         int Age { get; set; }
         string Email { get; set; }
-        ContractType Type { get; set; }
     }
 }
---
+++ Models/PhysicalContractModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechnicalAnalysis.Core.Interfaces;
+
+namespace TechnicalAnalysis.Models
+{
+    public class PhysicalContractModel : IContract 
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+    }
+}
