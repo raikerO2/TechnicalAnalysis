daa06fc916eaf684389e2c6d83fdbee01f56bb00
raikerO2 <raresmihaiiaut@gmail.com>
2021-04-25 01:10:01 +0100

    Added Upload prototype functionality

--- Controllers/UploadController.cs
+++ Controllers/UploadController.cs
@@ -2,85 +2,27 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using TechnicalAnalysis.Models;
 
 namespace TechnicalAnalysis.Controllers
 {
     public class UploadController : Controller
     {
+        [HttpGet]
         public IActionResult Upload()
         {
             return View();
         }
 
-        // GET: UploadController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
 
-        // GET: UploadController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: UploadController/Create
+        //
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<IActionResult> Upload(List<IFormFile> files)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: UploadController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: UploadController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: UploadController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: UploadController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            return Ok();
         }
     }
 }
